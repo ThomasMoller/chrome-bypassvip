38dd76aa6b3d2501f88c72287b45715c6061b226
ThomasMoller <tm@fonk.dk>
2022-11-04 02:44:49 +0100

    Popdown message now looks somewhat ok

--- scripts/bypassvip.js
+++ scripts/bypassvip.js
@@ -2,7 +2,16 @@ chrome.runtime.sendMessage({"cmd": 'getLink', "message": document.location.href}
     if(response.success) {    
         fetch(chrome.runtime.getURL('/popdown.html')).then(r => r.text()).then(html => {
             document.body.insertAdjacentHTML('afterbegin', html);
-            document.getElementById("bypassvipmessage").innerHTML = `👉 <a href="${response.destination}">${response.destination}</a>`;
+            document.getElementById("bypassviplink").innerHTML = `<a href="${response.destination}">${response.destination}</a>`;
+            document.getElementById("bypassvip-image").src = chrome.runtime.getURL("images/icon-48.png");
+            document.getElementById("bypassvip-close").addEventListener("click", function() {
+                document.getElementById('bypassvipmessage').className = 'bypassvip-slide-up';
+                setTimeout(() => {
+                    document.getElementById("bypassvipmessage").style.display = 'none';
+                  }, 500);
+            });
+
+            document.getElementById('bypassvipmessage').className = 'bypassvip-slide-down';
         });
     }
 });
